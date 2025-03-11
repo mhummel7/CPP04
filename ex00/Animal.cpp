32a9d40cc71a2bfa40a172ac92995d94a8d2caec
Manuel <mhummel@student.42heilbronn.de>
2025-03-11 12:35:44 +0100

    Create Animal.cpp
    Functions like constructor, destructor, gettype, makesound etc

---
+++ ex00/Animal.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 16:15:01 by mhummel           #+#    #+#             */
+/*   Updated: 2025/03/11 12:29:15 by mhummel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+
+Animal::Animal() : type("")
+{
+	std::cout << "Animal constructor called" << std::endl;
+}
+
+Animal::Animal(const Animal& other) : type(other.type)
+{
+	std::cout << "Animal copy constructor called" << std::endl;
+}
+
+Animal &Animal::operator=(const Animal& other)
+{
+	std::cout << "Animal assignment operator called" << std::endl;
+	if (this != &other)
+		type = other.type;
+	return (*this);
+}
+
+Animal::~Animal()
+{
+	std::cout << "Animal destructor called" << std::endl;
+}
+
+std::string Animal::getType() const
+{
+	return (type);
+}
+
+void Animal::makeSound() const
+{
+	std::cout << "* Generic animal sound *" << std::endl;
+}
