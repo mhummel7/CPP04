7e4a225177a9e41d3525339d51bbb039438b54ca
Manuel <mhummel@student.42heilbronn.de>
2025-03-12 11:05:49 +0100

    Upload Animal.hpp

---
+++ ex01/Animal.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 16:11:13 by mhummel           #+#    #+#             */
+/*   Updated: 2025/03/12 10:59:46 by mhummel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <string>
+# include <iostream>
+
+class Animal
+{
+	protected:
+		std::string type;
+
+	public:
+		// Constructors & Destructor
+		Animal();
+		Animal(const Animal& other);
+		Animal& operator=(const Animal& other);
+		virtual ~Animal();
+
+		// Member Functions
+		std::string getType() const;
+		virtual void makeSound() const;
+};
+
+#endif
